442dea2d4a14457c7cefb4124dd74ce9640a6d61
LoneCoala <75661290+LoneCoala@users.noreply.github.com>
2021-10-13 10:22:31 +0700

    Add files via upload

---
+++ ООП_Лабораторная_3/ООП_Лабораторная_3/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace ООП_Лабораторная_3
+{
+    partial class Сортировка
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Окно_Вывода = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Решение = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(17, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A";
+            this.label1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(17, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(17, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "C";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_KeyDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(38, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_KeyDown);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(38, 75);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(358, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Окно_Вывода
+            // 
+            this.Окно_Вывода.BackColor = System.Drawing.SystemColors.Control;
+            this.Окно_Вывода.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Окно_Вывода.ForeColor = System.Drawing.Color.Black;
+            this.Окно_Вывода.Location = new System.Drawing.Point(144, 23);
+            this.Окно_Вывода.Name = "Окно_Вывода";
+            this.Окно_Вывода.Size = new System.Drawing.Size(295, 121);
+            this.Окно_Вывода.TabIndex = 8;
+            this.Окно_Вывода.Click += new System.EventHandler(this.Окно_Вывода_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(20, 4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Исходные Данные";
+            // 
+            // Решение
+            // 
+            this.Решение.AutoSize = true;
+            this.Решение.ForeColor = System.Drawing.Color.Black;
+            this.Решение.Location = new System.Drawing.Point(141, 4);
+            this.Решение.Name = "Решение";
+            this.Решение.Size = new System.Drawing.Size(52, 13);
+            this.Решение.TabIndex = 10;
+            this.Решение.Text = "Решение";
+            // 
+            // Сортировка
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 182);
+            this.Controls.Add(this.Решение);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Окно_Вывода);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.Name = "Сортировка";
+            this.Text = "Сортировка";
+            this.Load += new System.EventHandler(this.Сортировка_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.Label Окно_Вывода;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Решение;
+    }
+}
+
---
+++ ООП_Лабораторная_3/ООП_Лабораторная_3/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ООП_Лабораторная_3
+{
+    public partial class Сортировка : Form
+    {
+        public Сортировка()
+        {
+            InitializeComponent();
+        }
+        private void Сортировка_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        double aNum = 0.0;
+        double bNum = 0.0;
+        double cNum = 0.0;
+
+        private void Окно_Вывода_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (Double.TryParse(textBox1.Text, out aNum) && Double.TryParse(textBox2.Text, out bNum) && Double.TryParse(textBox3.Text, out cNum))
+            {
+                if ((aNum >= bNum) && (aNum >= cNum))
+                {
+                    if (bNum >= cNum)
+                        Окно_Вывода.Text = "A=" + aNum + " B=" + bNum + " C=" + cNum;
+                    else
+                        Окно_Вывода.Text = "A=" + aNum + " C=" + cNum + " B=" + bNum; ;
+                }
+                else if ((bNum >= aNum) && (bNum >= cNum))
+                {
+                    if (aNum >= cNum)
+                        Окно_Вывода.Text = "B=" + bNum + " A=" + aNum + " C=" + cNum;
+                    else
+                        Окно_Вывода.Text = "B=" + bNum + " C=" + cNum + " A=" + aNum; ;
+                }
+                else if ((cNum >= aNum) && (cNum >= bNum))
+                {
+                    if (bNum >= aNum)
+                        Окно_Вывода.Text = "C=" + cNum + " B=" + bNum + " A=" + aNum;
+                    else
+                        Окно_Вывода.Text = "C=" + cNum + " A=" + aNum + " B=" + bNum; ;
+                }
+            }
+            else Окно_Вывода.Text = "Исходные данные не корректны или не полны";
+        }
+    }
+}
